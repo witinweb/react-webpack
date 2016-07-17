7fbfbd323ec4ca4aecb6799f9bbdcf1dff46c2de
SCHEMR <schemryj@gmail.com>
2016-07-17 20:45:10 +0900

    webpack-merge 설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     // Entry accepts a path or an object of entries.
     // We'll be using the latter form given it's
     // convenient with more complex configurations.
@@ -19,7 +20,20 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'React-Webpack'
+            title: 'Webpack demo'
         })
     ]
 };
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, {});
+        break;
+    default:
+        config = merge(common, {});
+}
+
+module.exports = config;
